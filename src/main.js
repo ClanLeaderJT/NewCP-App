7f6d6b12819855583d03263fb4f5e2fce97b6ed4
Jayy von Kush <jtman112@gmail.com>
2022-12-19 10:22:32 +1300

    Removed Single Instance
    added Multi Instance Capabilities for those who want to play with multiple penguins at once.

--- src/main.js
+++ src/main.js
@@ -86,38 +86,8 @@ const createWindow = () => {
   );
 };
 
-if (app.requestSingleInstanceLock()) {
-  app.on("second-instance", (_event, _commandLine, _workingDirectory) => {
-    // Someone tried to run a second instance, we should focus our window.
-    if (mainWindow) {
-      if (mainWindow.isMinimized()) mainWindow.restore();
-      mainWindow.focus();
-    }
-  });
 
   app.on("ready", createWindow);
 
   app.setAsDefaultProtocolClient("newcp");
 
-  // Quit when all windows are closed, except on macOS. There, it's common
-  // for applications and their menu bar to stay active until the user quits
-  // explicitly with Cmd + Q.
-  app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-      app.quit();
-      discord_client.disconnect();
-      process.exit(0); // because squirrel
-    }
-  });
-
-  app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
-    }
-  });
-} else {
-  app.quit();
-  process.exit(0); // because squirrel
-}
